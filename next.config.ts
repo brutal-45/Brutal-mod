fe5c53ef16aa6081ef4d45c1618d7aa60b7baa7c
brutal-45 <creatorsports81@gmail.com>
2026-05-05 19:17:29 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: false,
-  allowedDevOrigins: [
-    "preview-chat-5cd5104c-a85d-4f07-9075-91a0ce883c95.space-z.ai",
-    ".space-z.ai",
-  ],
+  reactStrictMode: true,
 };
 
 export default nextConfig;
+
